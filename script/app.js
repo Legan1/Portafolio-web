108891bb151b90eb20a941d78e3d918aeae7d99e
Legan1 <luisveveloper@gmail.com>
2022-09-21 19:29:35 -0700

    nueva actualizacion

--- script/app.js
+++ script/app.js
@@ -1,10 +1,12 @@
 
+
+
 //*writing */
 const text = document.querySelector(".sec-text")
 
 const textload =() =>{
     setTimeout(() => {
-        text.textContent='Luis Angel'
+        text.textContent=''
     }, 0);
     setTimeout(() => {
         text.textContent='diseñador'
@@ -30,48 +32,5 @@ window.addEventListener("load",()=>{
 })
 
 
-const grid = new Muuri('.grid', {
-	layout: {
-		rounding: false
-	}
-});
 
-window.addEventListener('load', () => {
-	grid.refreshItems().layout();
-	document.getElementById('grid').classList.add('.imagenes-cargadas');
 
-	// Agregamos los listener de los enlaces para filtrar por categoria.
-	const enlaces = document.querySelectorAll('#categorias a');
-	enlaces.forEach((elemento) => {
-		elemento.addEventListener('click', (evento) => {
-			evento.preventDefault();
-			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
-			evento.target.classList.add('activo');
-
-			const categoria = evento.target.innerHTML.toLowerCase();
-			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
-		});
-	});
-	// Agregamos listener para las imagenes
-	const overlay = document.getElementById('overlay');
-	document.querySelectorAll('.grid .item img').forEach((elemento) => {
-		elemento.addEventListener('click', () => {
-			const ruta = elemento.getAttribute('src');
-			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
-
-			overlay.classList.add('activo');
-			document.querySelector('#overlay img').src = ruta;
-			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
-		}); 
-	});
-
-	// Eventlistener del boton de cerrar
-	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
-		overlay.classList.remove('activo');
-	});
-
-	// Eventlistener del overlay
-	overlay.addEventListener('click', (evento) => {
-		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
-	});
-});
